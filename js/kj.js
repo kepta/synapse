99705ef7fe01b26400ee4b062dddf7b537872ef6
kepta <0o3ko0@gmail.com>
2014-12-05 15:35:27 +0530

    changes

--- js/kj.js
+++ js/kj.js
@@ -1,29 +1,73 @@
+
 $( document ).ready(function() {
     // your code here
-
-$(window).scroll(function() {
-		$('#animatedElement').each(function(){
+	//$('#othershit').addClass("slideUp");
+	
+	$(window).scroll(function() {
+		
+		$('#othershit').each(function(){
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
 			if (imagePos < topOfWindow+400) {
-				$(this).addClass("slideUp");
+			//	$(this).addClass("slideDown");
 			}
 		});
-	});
-	
-	
+		
+		$('#cyno').each(function(){
+		var imagePos = $(this).offset().top;
 
-	$('#othershit').hover(function() {
-		console.log("hi");
-		$(this).addClass("slideUp");
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+800) {
+				$(this).addClass("slideDown");
+			}
+		});
+		
+		$('#ramp').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+700) {
+				$(this).addClass("slideDown");
+			}
+		});
+		$('#piano').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+600) {
+				$(this).addClass("slideDown");
+			}
+		});
+		
+		$('#rock').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+600) {
+				$(this).addClass("slideDown");
+			}
+		});
+		$('#eventzone').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+500) {
+				$(this).addClass("slideDown");
+			}
+		});
+		
 	});
+	
+	
+$('circle').addClass("pulse");
 	
 	console.log("checl");
 	
 	
 	
 	$('circle').hover( function() {
+		console.log("checkkkk");
 		$(this).addClass("slideUp");
 	});
 	
