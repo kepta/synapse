4e943b038bc692bad2795785d7cf05c30edf5042
kepta <0o3ko0@gmail.com>
2014-12-05 13:53:13 +0530

    animate added

--- js/kj.js
+++ js/kj.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
-  
-  $(".show").click(function(){
-    $("#texty").show();
-  });
-});
+$( document ).ready(function() {
+    // your code here
+
+$(window).scroll(function() {
+		$('#animatedElement').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+	
+	
+
+	$('#othershit').hover(function() {
+		console.log("hi");
+		$(this).addClass("slideUp");
+	});
+	
+	console.log("checl");
+	
+	
+	
+	$('circle').hover( function() {
+		$(this).addClass("slideUp");
+	});
+	
+	});
